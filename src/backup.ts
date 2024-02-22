3175a4281d46d16c42b08f905a34d90f92fe7e06
Artemis84 Labs <88445072+artemis84@users.noreply.github.com>
2024-02-22 17:58:22 +0000

    revert some code

--- src/backup.ts
+++ src/backup.ts
@@ -71,7 +71,7 @@ export const backup = async () => {
 
   let date = new Date().toISOString();
   const timestamp = date.replace(/[:.]+/g, "-");
-  const filename = `database-backup/sql-${timestamp}.tar.gz`;
+  const filename = `backup-sql-${timestamp}.tar.gz`;
   const filepath = `/tmp/${filename}`;
 
   await dumpToFile(filepath);
